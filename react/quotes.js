786b10f365790895202793c254c2f60feb3df4e1
pradyunrao <riffreefer@gmail.com>
2022-03-10 16:50:04 +0530

    adding quotes file

---
+++ react/quotes.js
@@ -0,0 +1,50 @@
+const quotes = [
+  {
+    text: "Our posturings, our imagined self-importance, the delusion that we have some privileged position in the universe, are challenged by this point of pale light. Our planet is a lonely speck in the great enveloping cosmic dark.",
+    author: "Carl Sagan"
+  },
+  {
+    text: "The first principle is that you must not fool yourself and you are the easiest person to fool.",
+    author: "Richard P. Feynman"
+  },
+  {
+    text: "You're an idealist, and I pity you as I would the village idiot.",
+    author: "Stanley Kubrick"
+  },
+  {
+    text: "Look up at the stars and not down at your feet. Try to make sense of what you see, and wonder about what makes the universe exist. Be curious.",
+    author: "Stephen Hawking"
+  },
+  {
+    text: "There is no greater education than one that is self-driven.",
+    author: "Neil deGrasse Tyson"
+  },
+  {
+    text: "The chances of each of us coming into existence are infinitesimally small, and even though we shall all die some day, we should count ourselves fantastically lucky to get our decades in the sun.",
+    author: "Richard Dawkins"
+  },
+  {
+    text: "What can be asserted without evidence can be dismissed without evidence.",
+    author: "Christopher Hitchens"
+  },
+  {
+    text: "Generally, we admire the thing we are not.",
+    author: "Stephen Fry"
+  },
+  {
+    text: "True intelligence requires fabulous imagination",
+    author: "Ian McEwan"
+  },
+  {
+    text: "I haven't slept for ten days, because that would be too long.",
+    author: "Mitch Hedberg"
+  },
+  {
+    text: "Don't sweat the petty things and don't pet the sweaty things.",
+    author: "George Carlin"
+  },
+  {
+    text: "I passionately believe that's it's not just what you say that counts, it's also how you say it - that the success of your argument critically depends on your manner of presenting it.",
+    author: "Alain de Botton"
+  },
+]
